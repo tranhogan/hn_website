6ba2efaf8b1988e24f88c67e0c5c3dcd3a4c9ac4
Hogan <tranhogan@yahoo.com>
2022-04-06 20:07:44 -0500

    adding bootstrap

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import sys, os
-from flask import Flask
+from flask import Flask, send_from_directory, render_template
 from flask_cors import CORS
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', template_folder='flask-server/templates')
 
 @app.route("/")
 def index():
-    return app.send_from_directory(app.static_folder, 'base.html')
-
+    return render_template('base.html')
 
 @app.route("/appointments")
 def appointment():
-    return 'appointment'
+    return render_template('appointments.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
