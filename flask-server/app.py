b5bc77c4b1eaeb0cbc738351dadee7a75eec5510
Hogan <tranhogan@yahoo.com>
2022-04-06 19:24:57 -0500

    added html files

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,13 +1,23 @@
 import sys, os
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return app.send_from_directory(app.static_folder, 'base.html')
 
 
+@app.route("/appointments")
+def appointment():
+    return 'appointment'
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
-
-def create_app():     
-    app = Flask(__name__)
-    CORS(app)
-    return app
+# def create_app():     
+#     app = Flask(__name__)
+#     CORS(app)
+#     return app
